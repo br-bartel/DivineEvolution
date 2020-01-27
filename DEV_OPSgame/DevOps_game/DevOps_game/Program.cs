02d6cdfe79605b7451818c6646ee361a1691fccf
lenapuzzle <lenapuzzle@yahoo.com>
2020-01-27 15:29:27 -0800

    first commit

---
+++ DEV_OPSgame/DevOps_game/DevOps_game/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevOps_game
+{
+    class Game
+    {
+
+
+        static void Main(string[] args)
+        {
+            startGame();
+
+        }
+
+        static bool gameOver = false;
+
+        private static void gameLoop()
+        {
+            displayText();
+            string input = Console.ReadLine();
+
+        }
+        private static void startGame()
+        {
+            World.Add("Docks", new Location());
+
+            while (gameOver != true)
+            {
+                gameLoop();
+            }
+        }
+        private static void displayText()
+            {
+            Console.WriteLine("Test");
+            }
+
+
+        public static Dictionary<string, Location> World = new Dictionary<string, Location>();
+
+
+        public /*abstract*/ class Location
+        {
+
+            List <string>  validInputs = new List<string>();
+
+           // public abstract void chooseText();
+          
+
+        }
+
+     
+
+        public class State
+        {
+            int cycle;
+            string location;
+            Dictionary <string, bool> Conditions;
+        }
+    }
+}
