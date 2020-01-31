b9bf0b537c1691bfeff1a4c70b8618ec9c11a62e
Benjamin Bartel <Benjamin.Bartel@tlgclass.com>
2020-01-31 13:36:30 -0800

    Added render class

---
+++ DEV_OPSgame/DevOps_game/DevOps_game/Render.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevOps_game
+{
+    static class Render
+    {
+        private static void StoryDisplay()
+        {
+
+        }
+
+        private static void FlavorDisplay()
+        {
+
+        }
+
+        private static void MapDisplay()
+        {
+
+        }
+
+        private static void StatusDisplay()
+        {
+
+        }
+
+        internal static void MainScreen()
+        {
+            
+        }
+    }
+}
